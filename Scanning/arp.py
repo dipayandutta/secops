0ce2d36b0247f5cd5bfa51d21038500995da437b
Dipayan Dutta <inbox.dipayan@gmail.com>
2024-04-07 19:59:38 +0530

    ARP scanning using scapy

---
+++ Scanning/arp.py
@@ -0,0 +1,20 @@
+from scapy.all import Ether,ARP,srp 
+
+def arpScan():
+    broadcast = "FF:FF:FF:FF:FF:FF"
+    ether_layer=Ether(dst=broadcast)
+    ip_range="192.168.100.1/24"
+    arp_layer=ARP(pdst=ip_range)
+
+    packet=ether_layer/arp_layer
+
+    ans,unans= srp(packet,iface="eth0",timeout=2)
+
+    for snd,rcv in ans:
+        ip = rcv[ARP].psrc
+        mac = rcv[Ether].src
+        print("IP = ",ip,"MAC= ",mac)
+
+
+if __name__ == '__main__':
+    arpScan()
